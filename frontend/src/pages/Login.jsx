953b4499da32060a966e50d659eabe20b67129d8
Umesh Thakur <thakur.sonunet.sonu@gmail.com>
2025-01-31 23:19:28 +0530

    Added new files

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,38 +1,55 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
+
+const schema = z.object({
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+})
 const Login = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [errormessage, seterrormessage] = useState('')
 
-  const loginButton = async () => {
+const {register, handleSubmit, formState:{errors}}=useForm({
+  resolver: zodResolver(schema)
+})
+
+  const loginButton = async (data) => {
     try {
       const response = await axios.post('http://localhost:3000/user/login', {
-        email,
-        password
+        email: data.email,
+        password: data.password
       });
       console.log(response.data);
       if (response.data && response.data.token) { 
         navigate('/');
-      }else{
-        navigate('/login');
       }
     } catch (error) {
       console.log(error);
+      seterrormessage("invalid credentials")
     }
   }
 
   return (
     <div>
       <h1>Login</h1>
-      <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={loginButton}>Login</button>
+      <form onSubmit={handleSubmit(loginButton)}>
+        <input type="text" placeholder="Email" {...register("email")} />
+        {errors.email && <p>{errors.email.message}</p>}
+        <input type="password"  placeholder='Password' {...register("password")} />
+        {errors.password && <p>{errors.password.message}</p>}
+        <button type='submit'>Login</button>
+        {errormessage && <p>{errormessage}</p>}
+      </form>
+      
     </div>
   )
 }
 
 export default Login
+
 
